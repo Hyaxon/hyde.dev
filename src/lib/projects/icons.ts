e2d8a5b67a04430ece0d83437ff40e59b9129aa0
Hyde <108911977+Hyaxon@users.noreply.github.com>
2026-09-21 21:41:21 -0500

    created email widget (#4)

--- src/lib/projects/icons.ts
+++ src/lib/projects/icons.ts
@@ -1,16 +1,25 @@
 import type { ProjectCategory } from "./types";
+import robotics from "../../assets/pixel/project-types/robotics.png";
+import games from "../../assets/pixel/project-types/games.png";
+import roblox from "../../assets/pixel/project-types/roblox.png";
+import software from "../../assets/pixel/project-types/software.png";
+import iot from "../../assets/pixel/project-types/iot.png";
+import web from "../../assets/pixel/project-types/web.png";
+import tools from "../../assets/pixel/project-types/tools.png";
+import hackathons from "../../assets/pixel/project-types/hackathons.png";
+import experiments from "../../assets/pixel/project-types/experiments.png";
 
 // TODO: Replace default icons
 
 export const projectIcons: Record<ProjectCategory, string> = {
-  robotics: "/pixel/project-types/robotics.png",
-  "game-dev": "/pixel/project-types/games.png",
-  roblox: "/pixel/project-types/roblox.png",
-  networking: "/pixel/project-types/software.png",
-  iot: "/pixel/project-types/iot.png",
-  software: "/pixel/project-types/software.png",
-  web: "/pixel/project-types/web.png",
-  tools: "/pixel/project-types/tools.png",
-  hackathons: "/pixel/project-types/hackathons.png",
-  experiments: "/pixel/project-types/experiments.png",
+  robotics: robotics.src,
+  "game-dev": games.src,
+  roblox: roblox.src,
+  networking: software.src,
+  iot: iot.src,
+  software: software.src,
+  web: web.src,
+  tools: tools.src,
+  hackathons: hackathons.src,
+  experiments: experiments.src,
 };
